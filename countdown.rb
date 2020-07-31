719da371e32e7012673aafbf7adc350a33d2d88f
lisbeth estevez <estevez34545@gmail.com>
2020-07-31 01:27:49 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,15 +1,15 @@
-def countdown(number)
-  while number > 0
-  puts "#{number} SECOND(S)!"
-  number -= 1
-  return "HAPPY NEW YEAR!"
+def countdown(count)
+    while count >0
+      puts "#{count} seconds!"
+      count-=1
+    end
+    "0 left. HAPPY NEW YEAR!"
   end
-end
-def countdown_with_sleep(number)
-   while number > 0
-      puts "#{number} SECOND(S)!"
-      number -= 1
-      sleep(1) 
-   end
-   "HAPPY NEW YEAR!"
-end
+
+  def countdown_with_sleep(count)
+    while count > 0
+      count-=1
+      sleep(1)
+    end
+      puts "0 left. HAPPY NEW YEAR!"
+  end
